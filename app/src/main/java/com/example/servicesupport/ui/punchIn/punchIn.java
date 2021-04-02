707f9ab270928cee998ac040a5c394a87233b50f
Jigar <jigarmehta277@gmail.com>
2021-04-02 11:11:18 +0530

    Main activity commit

---
+++ app/src/main/java/com/example/servicesupport/ui/punchIn/punchIn.java
@@ -0,0 +1,38 @@
+package com.example.servicesupport.ui.punchIn;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.servicesupport.R;
+
+public class punchIn extends Fragment {
+
+    private PunchInViewModel mViewModel;
+
+    public static punchIn newInstance() {
+        return new punchIn();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.punch_in_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PunchInViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
